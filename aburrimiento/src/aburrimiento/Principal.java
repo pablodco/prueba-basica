e1aa43e704ac8d9fc7a2489117f2693998a82e4a
Pablo del Campo Olano <pablodel.campo@opendesuto.es>
2022-10-05 08:58:23 +0200

    Añadido print

---
+++ aburrimiento/src/aburrimiento/Principal.java
@@ -0,0 +1,11 @@
+package aburrimiento;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		persona persona= new persona("123","Pablo","del Campo","1");
+		System.out.println(persona.getNombre());
+	}
+
+}
